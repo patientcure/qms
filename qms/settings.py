4f95b25e1c7c0c06a149d124a848cc7bfda5140e
Yuvraj Singh <qomfortzone@gmail.com>
2025-09-17 19:40:15 +0530

    fixed code

--- qms/settings.py
+++ qms/settings.py
@@ -179,7 +179,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:5173",
     "https://*.devtunnels.ms",
-    "https://qms-2h5c.onrender.com"
+    "https://qms-2h5c.onrender.com",
     "http://69.62.80.202",
 ]
 
